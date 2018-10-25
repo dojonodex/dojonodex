a0c68b40b7b376c9fcfc67e9c51acdd8fb06fcb1
sansuns360 <sansuns360@gmail.com>
2018-10-25 12:13:06 -0400

    updated dojocal4 test pages

--- public/js/dojocal/dojocal4.js
+++ public/js/dojocal/dojocal4.js
@@ -8,28 +8,43 @@
         ], function(parser,  DateTextBox, NumberTextBox){
             parser.parse();
 
+
+
             var daysToAdd = 90;
             var date = new Date(2018, 0,  4);
             var fromDate = new DateTextBox({
-                value: date
+                value: date,
+                onChange: function(v){
+                    console.log("---------------- l@@K value changed ---------------",v);
+                    console.log(fromDate.getValue().toISOString());
+                    var xDate =  new Date(v.getFullYear(), v.getMonth(),   v.getDate() + addedDays.getValue() );
+                    console.log(xDate);
+                    toDate.setValue(xDate);
+                     
+                }
             },"fromDate");
             fromDate.startup();
+
+
             
             var date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate() + daysToAdd);
             var toDate = new DateTextBox({
-                value: date2
+                value: date2,
+                readOnly: true
             },"toDate"); 
             toDate.startup();
 
 
             var addedDays = new NumberTextBox({
                 value: daysToAdd,
+                constraints:{min:1,max:180,places:0},
                 onChange: function(v){
                     console.log("---------------- l@@K value changed ---------------",v);
                     console.log(fromDate.getValue().toISOString());
                     var xDate =  new Date(fromDate.getValue().getFullYear(), fromDate.getValue().getMonth(),   fromDate.getValue().getDate() + v );
                     console.log(xDate);
                     toDate.setValue(xDate);
+
                 }
             }, "addedDays");
             addedDays.startup();
@@ -37,7 +52,7 @@
             // on(addedDays, "change", function(){
             //     toDate.set("value",  date.getDate() + addedDays.getValue());
             // });
-
+           
    
         }); 
  
